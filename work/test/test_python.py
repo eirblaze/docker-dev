40fd50392e389ef07434f09e39a8e19b3e244252
eirblaze <eirblaze@live.jp>
2020-04-25 01:16:55 +0900

    グラフをカクカクにしてみた

--- work/test/test_python.py
+++ work/test/test_python.py
@@ -12,7 +12,7 @@ def func1(x):
 def func2(x0, x1):
     return itg.quad(func1, x0, x1)[0]
 
-x = pd.Series([i for i in np.arange(VMIN, VMAX, 0.01)])
+x = pd.Series([i for i in np.arange(VMIN, VMAX, 0.5)])
 y = x.map(func1)
 y_sum = pd.Series([func2(VMIN, i) for i in x])
 
